ac0c5459ae62c4c928b956df20497015a8e0b11a
zerebom <ice.choco.pudding.kokoro@gmail.com>
2024-03-23 17:09:43 +0900

    Fix typo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,16 +43,14 @@ const cx = (...classes: any[]) => classes.filter(Boolean).join(' ');
 
 export default function RootLayout({
   children,
-  maxWidthClass = 'max-w-3xl',
 }: Readonly<{
   children: React.ReactNode;
-  maxWidthClass?: string;
 }>) {
   return (
     <html lang='ja' className={cx(RobotoSlabFont.className)}>
       <head></head>
       <body
-        className={`antialiased ${maxWidthClass} mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto`}
+        className={`antialiased 'max-w-3xl' mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto`}
       >
         <main>
           <Navbar />
